d1e377b95b4ad058f86040d0bbbc5fe8d73e4476
tuanvu2642k2 <tuanvu2642k2@gmail.com>
2022-01-17 17:56:44 +0700

    ghi chu

--- Bai 2/giaiphuongtrinh.java
+++ Bai 2/giaiphuongtrinh.java
@@ -4,23 +4,23 @@ public class giaiphuongtrinh {
     public static void main(String[] args) {
         double a, b, c, x1, x2, delta;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập a (a # 0): ");
+        System.out.print("Nhap a (a # 0): ");
         a = scanner.nextDouble();
-        System.out.print("Nhập b: ");
+        System.out.print("Nhap b: ");
         b = scanner.nextDouble();
-        System.out.print("Nhập c: ");
+        System.out.print("Nhap c: ");
         c = scanner.nextDouble();
-        System.out.println("Phương trình bậc hai bạn vừa nhập có dạng: " + a + "x^2 + " + b + "x + " + c + " = 0");
+        System.out.println("Phuong trinh bac 2 vua nhap co dang: " + a + "x^2 + " + b + "x + " + c + " = 0");
         delta = Math.pow(b, 2) - 4 * a * c;
         if (delta < 0) {
-            System.out.println("Phương trình vô nghiệm!");
+            System.out.println("Phuong trinh vo nghiem!");
         } else if (delta == 0) {
             x1 = -b / (2 * a);
-            System.out.println("Phương trinh có 1 nghiệm là x1 = x2 = " + x1);
+            System.out.println("Phuong trinh co nghiem kep x1 = x2 = " + x1);
         } else {
             x1 = (-b + Math.sqrt(delta)) / (2 * a);
             x2 = (-b - Math.sqrt(delta)) / (2 * a);
-            System.out.println("Phương trình có 2 nghiệm x1 = " + x1 + " và x2 = " + x2);
+            System.out.println("Phuong trinh co hai nghiem x1 = " + x1 + " và x2 = " + x2);
         }
     }
 }
