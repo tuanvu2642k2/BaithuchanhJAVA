e0168503ba831c0260c40d21d00272cc78afe0cd
tuanvu2642k2 <tuanvu2642k2@gmail.com>
2022-02-21 18:53:21 +0700

    bai thuc hanh ngay 21-2-2022

---
+++ bai thuc hanh ngay 21-2-2022/bai40.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class bai40 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int ngay = sc.nextInt();
+
+        switch (ngay) {
+            case 0:
+                System.out.println("Chu nhat");
+                break;
+            case 1:
+                System.out.println("Thu hai");
+                break;
+            case 2:
+                System.out.println("Thu ba");
+                break;
+            case 3:
+                System.out.println("Thu tu");
+                break;
+            case 4:
+                System.out.println("Thu nam");
+                break;
+            case 5:
+                System.out.println("Thu sau");
+                break;
+            case 6:
+                System.out.println("Thu bay");
+                break;
+            default:
+                System.out.println("nhap sai ngay");
+        }
+
+        sc.close();
+
+    }
+}
