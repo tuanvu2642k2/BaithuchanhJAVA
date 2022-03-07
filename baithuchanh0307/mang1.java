ad31bbca143fd42cadd9a4b241b8d66af7a3d12e
tuanvu2642k2 <tuanvu2642k2@gmail.com>
2022-03-07 16:23:15 +0700

    bai thuc hanh 7-3

---
+++ baithuchanh0307/mang1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class mang1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap so phan tu");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int a, sum = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println("nhap phan tu thu " + i + ": ");
+            a = sc.nextInt();
+            arr[i] = a;
+            if (a % 2 == 0) {
+                sum += a;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("tong phan tu chan: " + sum);
+        sc.close();
+
+    }
+}
